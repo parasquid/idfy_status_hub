ea9a7576367c24bb47b848019d1697b79fbe018e
parasquid <tristan.gomez@gmail.com>
2015-11-23 04:27:18 +0800

    include image in the pdf downlod

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'will_paginate'
 gem 'will_paginate-bootstrap'
 gem 'httparty'
 gem 'prawn'
+gem 'humanize_boolean'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,20 @@
+require 'open-uri'
 class MembersController < ApplicationController
+  include ActionView::Helpers::DateHelper
   def show
     @member = Member.find(params[:id])
-    filename = "/tmp/membber-card-#{@member.id_api}.pdf"
-    Prawn::Document.generate(filename) do
-      text "Hello World!"
-    end
+    filename = "/tmp/member-card-#{@member.id_api}.pdf"
+    Prawn::Document.generate(filename) do |pdf|
+      pdf.image open(@member.image)
+      pdf.move_down 10
+      pdf.text "Ethnicity: #{@member.ethnicity_string}"
+      pdf.text "Status: #{@member.status}"
+      pdf.text "Date of Birth: #{@member.dob} (#{time_ago_in_words(@member.dob)} old)"
+      pdf.text "Weight: #{@member.weight_in_kg} kg"
+      pdf.text "Height: #{@member.height} cm"
+      pdf.text "Vegan: #{@member.is_veg.humanize}"
+      pdf.text "Drink: #{@member.drink.humanize}"
+     end
     send_file filename
   end
 end
