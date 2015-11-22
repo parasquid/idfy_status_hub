cfda034bdd780c96bd0a6c035a2d5f79ff3a9a6e
parasquid <tristan.gomez@gmail.com>
2015-11-22 23:44:58 +0800

    add ethicity look up table

---
+++ config/initializers/ethnicity_lut.rb
@@ -0,0 +1,12 @@
+ETHNICITY_LUT = {
+  0 => :asian,
+  1 => :indian,
+  2 => :african_american,
+  3 => :asian_american,
+  4 => :european,
+  5 => :british,
+  6 => :jewish,
+  7 => :latino,
+  8 => :native_american,
+  9 => :arabic
+}
